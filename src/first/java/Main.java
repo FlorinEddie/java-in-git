c6bed809175403f398326ef69a8287e15ee899a1
FlorinG <eddie07_ro@yahoo.com>
2018-12-15 15:21:32 +0200

    third project commit

--- src/first/java/Main.java
+++ src/first/java/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My first project in git1");
         System.out.println("Primul meu proiect in Git");
+        System.out.println("Un mesaj");
     }
 }
